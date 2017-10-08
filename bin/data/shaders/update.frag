447873554243f6febc171ffedbc2285e4a20afbe
Shinichiro Takata <shinichiro-takata@united.jp>
2017-10-08 22:34:13 +0900

    Change my settings

--- bin/data/shaders/update.frag
+++ bin/data/shaders/update.frag
@@ -15,9 +15,10 @@ uniform float u_timestep;
 uniform float u_scale;
 uniform vec3 u_emitterPos;
 uniform vec3 u_prevEmitterPos;
+uniform vec2 u_resolution;
 
 void main(void){
-    vec2 st = gl_TexCoord[0].st;
+    vec2 st = gl_TexCoord[0].st;  // 0〜テクスチャのピクセル数
     // 前の位置情報とパーティクル初期化からの経過時間を取得
     vec4 posAndAge = texture2DRect(u_posAndAgeTex,st);
     // 前の速度と生存期間を取得
@@ -38,8 +39,25 @@ void main(void){
         float theta = 2.0 * PI * random(pos.yy);
         float phi = PI * random(pos.zz);
         float r = 5.0 * random(pos.xy);
-        vec3 startPos = u_prevEmitterPos + (u_emitterPos - u_prevEmitterPos) * random(pos.yz);
+
+        vec3 startPos = vec3(0.0);
+
+
+        // Traiangle Start
+        float xoffset = -(1.0/3.0 * u_resolution.x)/2.0;
+        float yoffset = -(1.0/2.0 * u_resolution.x)/2.0;
+        if(st.x < (u_resolution.x / 3.0)) {
+            //float xoffset = -(1.0/3.0 * u_resolution.x)/2.0;
+            //float yoffset = -(1.0/2.0 * u_resolution.x)/2.0;
+            startPos = vec3(st.x - (u_resolution.x / 2.0) - xoffset, -1.73 * st.x * sin(1.0/3.0 * PI) - yoffset, 0.0);
+        }else if(st.x >= (u_resolution.x / 3.0) && st.x < ((u_resolution.x / 3.0) * 2.0)){
+            startPos = vec3(st.x - (u_resolution.x / 2.0) - xoffset, 1.73 * st.x * sin(1.0/3.0 * PI) -u_resolution.x - yoffset, 0.0);
+        }else{
+            startPos = vec3(2.0 * st.x - u_resolution.x / 2.0 - 2.0/3.0 * u_resolution.x - 2.0/ 3.0 * u_resolution.x - xoffset, -yoffset, 0.0);
+        }
+
         pos = startPos + vec3(r * sin(theta) * cos(phi), r * sin(theta) * sin(phi), r * cos(theta));
+        
         vel.xyz = vec3(normalize(startPos));
     }
     
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -10,7 +10,7 @@ void ofApp::setup(){
     
     //パーティクルの数を設定
 
-    particleNum = 1000000;
+    particleNum = 500000;
     texRes = ceil(sqrt(particleNum));
     
     // レンダリング用シェーダーの読み込み
@@ -30,7 +30,8 @@ void ofApp::setup(){
             if(index < particleNum){
                 particles.addVertex(ofVec3f(0,0,0));
                 particles.addTexCoord(ofVec2f(i, j)); // Fboのテクスチャー内で、データを読み出す位置を設定
-                particles.addColor(ofFloatColor(1.0,1.0,1.0,0.5));
+                //particles.addColor(ofFloatColor(1.0,1.0,1.0,0.5));
+                particles.addColor(ofFloatColor(1.0,1.0,ofRandom(0, 0.8),0.5));
             }
         }
     }
@@ -46,9 +47,12 @@ void ofApp::setup(){
     for (int x = 0; x < texRes; x++){
         for (int y = 0; y < texRes; y++){
             int i = texRes * y + x;
-            posAndAge[i*4 + 0] = ofRandom(-1.0,1.0);
-            posAndAge[i*4 + 1] = ofRandom(-1.0,1.0);
-            posAndAge[i*4 + 2] = ofRandom(-1.0,1.0);
+            //posAndAge[i*4 + 0] = ofRandom(-0.2,0.2);
+            //posAndAge[i*4 + 1] = ofRandom(-0.2,0.2);
+            //posAndAge[i*4 + 2] = ofRandom(-0.2,0.2);
+            posAndAge[i*4 + 0] = ofRandom(1.0);
+            posAndAge[i*4 + 1] = 0.0;
+            posAndAge[i*4 + 2] = 0.0;
             posAndAge[i*4 + 3] = 0;
         }
     }
@@ -66,7 +70,8 @@ void ofApp::setup(){
             velAndMaxAge[i*4 + 0] = 0.0;
             velAndMaxAge[i*4 + 1] = 0.0;
             velAndMaxAge[i*4 + 2] = 0.0;
-            velAndMaxAge[i*4 + 3] = ofRandom(1,150);
+            //velAndMaxAge[i*4 + 3] = ofRandom(1,150);
+            velAndMaxAge[i*4 + 3] = 0.1;
         }
     }
 
@@ -87,7 +92,7 @@ void ofApp::update(){
     // パーティクルの発生位置を更新
 
     prevEmitterPos = emitterPos;
-    emitterPos = 300 * ofVec3f(ofSignedNoise(time, 0, 0),ofSignedNoise(0, time, 0),ofSignedNoise(0, 0, time));
+    emitterPos = 200 * ofVec3f(ofSignedNoise(time, 0, 0),ofSignedNoise(0, time, 0),ofSignedNoise(0, 0, time));
     
     // パーティクルの位置を計算
 
@@ -107,10 +112,11 @@ void ofApp::update(){
 
     updatePos.setUniformTexture("u_velAndMaxAgeTex", pingPong.src->getTextureReference(1), 1);
     updatePos.setUniform1f("u_time", time);
-    updatePos.setUniform1f("u_timestep", 0.5);
+    updatePos.setUniform1f("u_timestep", 0.1);
     updatePos.setUniform1f("u_scale", 0.005);
     updatePos.setUniform3f("u_emitterPos", emitterPos.x, emitterPos.y, emitterPos.z);
     updatePos.setUniform3f("u_prevEmitterPos", prevEmitterPos.x, prevEmitterPos.y, prevEmitterPos.z);
+    updatePos.setUniform2f("u_resolution", texRes, texRes);
     pingPong.src->draw(0, 0);
     updatePos.end();
     pingPong.dst->end();
@@ -138,9 +144,9 @@ void ofApp::draw(){
         ofPushStyle();
         ofEnableBlendMode(OF_BLENDMODE_ALPHA);
         pingPong.dst->getTextureReference(0).draw(0,0);
-        ofDrawBitmapStringHighlight("Position", 0,14);
+        //ofDrawBitmapStringHighlight("Position", 0,14);
         pingPong.dst->getTextureReference(1).draw(texRes,0);
-        ofDrawBitmapStringHighlight("Velocity", texRes,14);
+        //ofDrawBitmapStringHighlight("Velocity", texRes,14);
         ofPopStyle();
     }
     ofDrawBitmapStringHighlight(ofToString(ofGetFrameRate()), 0,ofGetHeight() - 2);
